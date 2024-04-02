76574e59543a578a59c841478c7e30294a215812
alembicbrian <alembicbrian@gmail.com>
2024-04-02 20:57:24 +0300

    lesson2

---
+++ web/main.dart
@@ -0,0 +1,70 @@
+// void main() {
+
+
+
+
+void add(int num1, int num2){
+  int sum = num1 + num2;
+   print("The sum is $sum");
+}
+void main(){
+  add(10, 20);
+}
+
+
+
+
+
+// // writing function outside main function.
+// void printName(){
+//   print("My name John James");
+// }
+// // this is our main function.
+// void main(){
+//   printName();
+// }
+
+// int num1=10;
+//  int num2=3;
+ 
+//  // performing arithmetic calculation
+//  int sum=num1+num2;       // addition
+//  int diff=num1-num2;      // subtraction
+//  int subtract = num2-num1;    // unary minus  
+//  int mul=num1*num2;       // multiplication
+//  double div=num1/num2;    // division
+//  int div2 =num1~/num2;     // integer division
+//  int mod=num1%num2;       // show remainder
+ 
+// //Printing info 
+//  print("The addition is $sum.");
+//  print("The subtraction is $diff.");
+//  print("The unary minus is $subtract");
+//  print("The multiplication is $mul.");
+//  print("The division is $div.");
+//  print("The integer division is $div2.");
+//  print("The modulus is $mod."); 
+  // String runesString = "Runes in Dart: \u{1F600} \u{1F64B} \u{1F68}";
+
+  // // Print the string
+  // print(runesString);
+  // // Creating a Map with String keys and int values
+  // Map<String, int> ages = {
+  //   'Alice': 30,
+  //   'Bob': 25,
+  //   'Charlie': 35,
+  // };
+  // print("Ages of students: $ages");
+
+  // Map<int, String> position = {1: "mike", 5: "ben",};
+
+  // print("$position");
+
+// List<String> names = ["John", "James", "Peter"];
+// print("Value of names is $names");
+// print("Value of names[0] is ${names[0]}"); // index 0
+// print("Value of names[1] is ${names[1]}"); // index 1
+// print("Value of names[2] is ${names[2]}"); // index 2
+
+// print(names);
+// }
